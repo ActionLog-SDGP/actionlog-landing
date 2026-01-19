2525c38b44f9cab42e9eaaf133b4a95ca8babdad
ThejanNim <thejannims@gmail.com>
2026-01-19 10:27:19 +0530

    fix: change footer content.

--- src/components/Sections/Banner/Banner.tsx
+++ src/components/Sections/Banner/Banner.tsx
@@ -31,7 +31,7 @@ export default function Banner() {
           />
           <div className="absolute inset-0 -top-32 md:-top-40 flex flex-col items-center justify-center">
             <h1 className="text-white text-4xl md:text-7xl font-medium tracking-tighter max-w-xs md:max-w-xl text-center">
-              Thinking. Drafting. Publishing
+              Transcribe. Analyze. Automate
             </h1>
             <div className="absolute bottom-10 flex flex-col items-center justify-center gap-2">
               <a
